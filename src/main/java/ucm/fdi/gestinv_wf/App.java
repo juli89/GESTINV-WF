9a9a80ffda7f25d1893c9c540fc2977592bc8436
juli89 <36840750+juli89@users.noreply.github.com>
2018-10-09 23:16:30 +0200

    Add files via upload

---
+++ src/main/java/ucm/fdi/gestinv_wf/App.java
@@ -0,0 +1,13 @@
+package ucm.fdi.gestinv_wf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
